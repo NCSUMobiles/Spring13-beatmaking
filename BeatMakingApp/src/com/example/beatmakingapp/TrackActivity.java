8a462f1ebd622518005c0cd982a4f89f89415616
nabajyoti1987 <npatowa@ncsu.edu>
2013-04-07 14:45:34 -0400

    Added menu items for the track screen

--- BeatMakingApp/src/com/example/beatmakingapp/TrackActivity.java
+++ BeatMakingApp/src/com/example/beatmakingapp/TrackActivity.java
@@ -10,6 +10,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
@@ -206,4 +207,10 @@ public class TrackActivity extends Activity {
 	            }
 	        }).create().show();
 	}
+	@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 }
